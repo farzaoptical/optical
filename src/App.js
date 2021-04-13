c4311aa20e9ac14f5c00c81cb7399a0ea39ad477
farzaoptical <82469732+farzaoptical@users.noreply.github.com>
2021-04-13 22:27:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import logo from "./لوجو فرزة.jpg";
+import data from "./data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  const branchesArray = data.المحلات;
+  const branches = branchesArray.map((branche) => (
+    <div key={branche.الرقم} className="branches-container">
+      <i className="fas fa-store"></i>
+      <p className="place">{branche["إسـم الـمـحــل"]}</p>
+      <p>{branche.العنوان}</p>
+      <div className="branches-info">
+        <a href={"tel:" + branche["رقم الهاتف"]}>
+          <i className="fab fa-whatsapp"></i>
         </a>
-      </header>
+        <a href={"tel:" + branche["رقم  الجوال"]}>
+          <i className="fas fa-phone"></i>
+        </a>
+        <a href={branche["لوكيشن الفرع"]}>
+          <i className="fas fa-map-marker-alt"></i>
+        </a>
+      </div>
+      <p>{branche["دوام رمضان"]}</p>
+    </div>
+  ));
+  return (
+    <div className="root">
+      <div className="img-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <main>{branches}</main>
     </div>
   );
 }
